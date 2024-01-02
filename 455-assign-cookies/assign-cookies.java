b5bb520e106995ba95c341fbb9eabe8cca8ce68e
Rohit Mhatre <101646263+rohit-mhatre@users.noreply.github.com>
2024-01-01 22:24:47 -0500

    Time: 10 ms (15.42%) | Memory: 45 MB (28.82%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,22 +1,15 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        int cookie = s.length;
-        if (cookie == 0){
-            return 0;
-        }
-        int count = 0;
         Arrays.sort(g);
         Arrays.sort(s);
-        int cookieIndex = cookie - 1;
-        int childIndex = g.length - 1;
-
-        while (cookieIndex >= 0 && childIndex >= 0) {
+        int cookieIndex = 0;
+        int childIndex = 0;
+        while (cookieIndex < s.length && childIndex < g.length) {
             if (s[cookieIndex] >= g[childIndex]) {
-            count++;
-            cookieIndex--;
+                childIndex++;
         }
-        childIndex--;
+        cookieIndex++;
         }
-    return count; 
+    return childIndex; 
     }
 }
