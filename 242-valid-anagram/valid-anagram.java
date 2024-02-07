fd2104e35718b652c46e63184c4c513b2c66add1
Rohit Mhatre <101646263+rohit-mhatre@users.noreply.github.com>
2024-02-07 06:54:33 -0500

    Time: 1 ms (99.70%) | Memory: 43 MB (75.20%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,11 +1,29 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         char[] s_char = s.toCharArray();
+//         char[] t_char = t.toCharArray();
+
+//         Arrays.sort(s_char);
+//         Arrays.sort(t_char);
+
+//         return Arrays.equals(s_char, t_char);
+//     }
+// }
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] s_char = s.toCharArray();
-        char[] t_char = t.toCharArray();
-
-        Arrays.sort(s_char);
-        Arrays.sort(t_char);
-
-        return Arrays.equals(s_char, t_char);
+        int[] count = new int[26];
+        for (char x : s.toCharArray()) {
+            count[x - 'a']++;
+        }
+        for (char x : t.toCharArray()) {
+            count[x - 'a']--;
+        }
+        for (int a : count) {
+            if (a != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
